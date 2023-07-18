68e49ec61f28bc324d465e9cde3c84c333696c5c
raldone01 <raldone01@gmail.com>
2023-07-18 14:56:48 +0200

    Pinger with gui support.

--- README.md
+++ README.md
@@ -1,8 +1,69 @@
-# Pinger
+# Server Pinger
 
-python pinger.py www.google.com --csv results.csv
+This project contains a python program which periodically pings a server, logs the response times and can also visualize this data in real-time.
 
-TODO
-* add help section
-* fix recall close instantly
-* fix plot  unresponsive
+It is a useful tool for network administrators and enthusiasts who want to continuously monitor a server's network performance and understand any network latency or disruption issues. The results can be stored in a CSV file, enabling long-term data analysis.
+
+## Table of Contents
+
+- [Server Pinger](#server-pinger)
+  - [Table of Contents](#table-of-contents)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+
+## Requirements
+
+This project uses the following Python packages:
+
+- argparse
+- csv
+- time
+- ping3
+- PySide6
+- matplotlib
+- socket
+- datetime
+- signal
+
+Python version 3.7 or above is required. The project may not work as expected with older versions.
+
+## Installation
+
+Before running the script, make sure to install all the required dependencies. It is recommended to create a new virtual environment for this. Here is an example using venv:
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+The `requirements.txt` file should contain all the necessary packages. As of my current knowledge cutoff in September 2021, I don't have the specific versions used in this project. You may want to ask the project author for the specific package versions.
+
+## Usage
+
+```bash
+python main.py [server] [--recall] [-n] [--csv] [--headless]
+```
+where:
+- `server`: The server to ping.
+- `--recall`: If used, previously recorded results will be recalled from the specified CSV file.
+- `-n`: The interval between pings in seconds. Default is 30 seconds.
+- `--csv`: The CSV file to write results to.
+- `--headless`: If used, the program will run without a GUI.
+
+If you don't specify a server, the program won't start the ping process.
+
+For example, to ping google.com every 5 seconds and store the result in a file named `ping_result.csv`, you can run:
+```bash
+python main.py google.com -n 5 --csv ping_result.csv
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue if you find a bug or a pull request for bug fixes, features or improvements.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 or later. See the `LICENSE` file for details.
--- pinger.py
+++ pinger.py
@@ -8,7 +8,7 @@ from PySide6.QtWidgets import QApplication
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from socket import gethostbyname
-from datetime import datetime
+from datetime import datetime, timedelta
 from matplotlib.dates import DateFormatter, date2num, num2date
 from matplotlib.ticker import MaxNLocator, FuncFormatter
 import matplotlib
@@ -20,17 +20,32 @@ matplotlib.use('Qt5Agg')
 class Pinger(QtCore.QObject):
     newDataSignal = QtCore.Signal(list)
 
-    def __init__(self, server, interval=30, csv_file=None):
+    def __init__(self, server, interval=30, csv_file=None, recall=False):
         super().__init__()
         self.server = server
-        self.ip = gethostbyname(self.server)
+        self.ip = gethostbyname(self.server) if self.server else None
         self.interval = interval
         self.csv_file = csv_file
         self.ping_results = []
+        self.recall = recall
         self.running = True
+
+        if self.recall:
+            self.recall_results()
+
+    def stoppable_sleep(self, ms):
+        max_wait = 100
+        while ms > 0 and self.running:
+            time.sleep(min(ms, max_wait) / 1000)
+            ms -= max_wait
 
     @QtCore.Slot()
     def start(self):
+        if self.recall:
+            self.newDataSignal.emit(self.ping_results)
+
+        if self.server is None:
+            return
         while self.running:
             timestamp = datetime.now()  # Use Python datetime here
             try:
@@ -41,9 +56,7 @@ class Pinger(QtCore.QObject):
                 success = 0
 
             response_time_ms = None if response_time is None else round(response_time * 1000)
-            # convert timestamp to unix
-            timestamp = timestamp.timestamp()
-            result = (timestamp, self.ip, success, response_time_ms)
+            result = (timestamp.timestamp(), self.ip, success, response_time_ms)
             print(f"{timestamp};{self.ip};{success};{response_time_ms}")
             self.ping_results.append(result)
 
@@ -51,31 +64,48 @@ class Pinger(QtCore.QObject):
                 with open(self.csv_file, 'a', newline='') as f:
                     writer = csv.writer(f, delimiter=';')
                     writer.writerow(result)
-
-            time.sleep(self.interval)
-
             self.newDataSignal.emit(self.ping_results)
+
+            self.stoppable_sleep(self.interval * 1000)
+
+
+    def recall_results(self):
+        with open(self.csv_file, 'r', newline='') as f:
+            reader = csv.reader(f, delimiter=';')
+            for row in reader:
+                timestamp = float(row[0])
+                result = (timestamp, row[1], int(row[2]), int(row[3]))
+                self.ping_results.append(result)
+                print(f"{datetime.fromtimestamp(timestamp)};{row[1]};{row[2]};{row[3]}")
 
     def stop(self):
         self.running = False
 
 
 class MyMplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=10, height=8, dpi=100):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
+        self.axes.set_ylabel('Response Time (Milliseconds)')
+        self.axes.set_xlabel('Time')
+        self.axes.grid(True)
+        self.axes.set_title('Server Pinger')
+        self.fig.tight_layout()
+        self.hl, = self.axes.plot([], [], 'b')
         super().__init__(self.fig)
 
     def update_figure(self, ping_results):
-        self.axes.clear()
-        self.axes.grid(True)
+        if not ping_results:
+            return
 
-        timestamps, _, successes, response_times = zip(*ping_results)
-        timestamps = [date2num(t) for t in timestamps]  # Convert datetime to Matplotlib's internal representation
+        timestamps, ips, successes, response_times = zip(*ping_results)
+        timestamps = [datetime.fromtimestamp(t) for t in timestamps]  # Convert datetime to Matplotlib's internal representation
+
+        self.axes.set_title(f"{ips[0]} - {timestamps[0]}")
 
         def format_func(x, pos):
             dt = num2date(x)
-            if max(timestamps) - min(timestamps) <= 60:
+            if max(timestamps) - min(timestamps) <= timedelta(seconds=60):
                 return dt.strftime('%H:%M:%S')
             else:
                 return dt.strftime('%H:%M')
@@ -83,29 +113,40 @@ class MyMplCanvas(FigureCanvas):
         self.axes.xaxis.set_major_locator(MaxNLocator(10))
         self.axes.xaxis.set_major_formatter(FuncFormatter(format_func))
 
-        for t, s in zip(timestamps, successes):
-            if s == 0:
-                self.axes.axvline(x=t, color='r', linewidth=2)
-        self.axes.plot(timestamps, response_times, 'b')
+        # mark failed pings with red zones
+        # pings are not equally spaced compute the span to the next ping
+        global app_args
+        for i in range(len(successes)):
+            if successes[i] == 0:
+                start = timestamps[i]
+                end = timestamps[i+1] if i < len(successes) - 1 else timestamps[i] + timedelta(seconds=app_args.n)
+                self.axes.axvspan(start, end, alpha=0.2, color='red')
+
+        self.hl.set_data(date2num(timestamps), response_times)
+        self.axes.relim()
+        self.axes.autoscale_view()
+
         self.draw()
 
+app_args = None
 
 def main():
     parser = argparse.ArgumentParser(description='Ping a server periodically and plot the response time.')
-    parser.add_argument('server', type=str, help='The server to ping.')
+    parser.add_argument('server', type=str, nargs='?', default=None, help='The server to ping.')
+    parser.add_argument('--recall', action='store_true', help='Recall recorded results.')
     parser.add_argument('-n', type=int, default=30, help='The interval between pings in seconds.')
     parser.add_argument('--csv', type=str, help='The CSV file to write results to.')
     parser.add_argument('--headless', action='store_true', help='Run without a GUI.')
     args = parser.parse_args()
+    global app_args
+    app_args = args
 
     app = QApplication([])
     app.setApplicationDisplayName("Server Pinger")
 
-    pinger = Pinger(args.server, args.n, args.csv)
+    pinger = Pinger(args.server, args.n, args.csv, args.recall)
     pinger_thread = QtCore.QThread()
     pinger.moveToThread(pinger_thread)
-    pinger_thread.started.connect(pinger.start)
-    pinger_thread.start()
 
     timer = QTimer()
     timer.start(500)  # You may change this if you wish.
@@ -118,6 +159,9 @@ def main():
     else:
         app.setQuitOnLastWindowClosed(False)
 
+    pinger_thread.started.connect(pinger.start)
+    pinger_thread.start()
+
     def on_quit(*args):
         app.quit()
         pinger.stop()
@@ -125,11 +169,7 @@ def main():
         pinger_thread.wait()
     signal.signal(signal.SIGINT, on_quit)
     app.exec()
-
-    print("event loop finished")
-
     on_quit()
-
 
 if __name__ == "__main__":
     main()
